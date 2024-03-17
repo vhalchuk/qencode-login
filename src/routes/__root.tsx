08aee81875bc8ce6896088c0b457b8fdcae70962
Volodymyr Halchuk <volodymyr.halchuk.dev@gmail.com>
2024-03-17 21:02:27 +0400

    Fix screen height on mobile devices

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -4,7 +4,7 @@ import QencodeLogo from "~/shared/svg-icons/qencode-logo.tsx";
 
 export const Route = createRootRoute({
   component: () => (
-    <main className="flex h-screen items-center justify-center p-4">
+    <main className="flex min-h-screen items-center justify-center p-4">
       <div className="m-4 flex w-full max-w-[400px] flex-col gap-y-20">
         <QencodeLogo className="self-center" />
         <Outlet />
